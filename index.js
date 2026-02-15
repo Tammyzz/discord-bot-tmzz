1f458986ce2d65c97ef663f83acecf36a0ebcae1
Tammyzz <anhtrantam2611@gmail.com>
2026-02-15 12:54:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log(`Bot đã online: ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
 
