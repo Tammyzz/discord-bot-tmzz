389951e32bab52129230ea215e8657c0a9227efb
Tammyzz <anhtrantam2611@gmail.com>
2026-02-15 14:03:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,24 +5,20 @@ app.get('/', (req, res) => {
   res.send('Bot đang sống 😎');
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Web server đang chạy');
 });
 
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.once('ready', () => {
   console.log(`Bot đã online: ${client.user.tag}`);
 });
 
-client.on('error', console.error);
-console.log("TOKEN =", process.env.TOKEN);
 client.login(process.env.TOKEN)
   .then(() => console.log("Đang login vào Discord..."))
   .catch(err => console.error("Lỗi login:", err));
